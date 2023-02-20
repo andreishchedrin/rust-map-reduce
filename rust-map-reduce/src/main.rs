7cb6fc2af2f1f8dd5510c8194ff2b4ad1a17070c
andreishchedrin <schedrin-post@rambler.ru>
2023-02-20 21:13:34 +0300

    csv

--- rust-map-reduce/src/main.rs
+++ rust-map-reduce/src/main.rs
@@ -1,3 +1,13 @@
+use std::{
+    thread,
+    error::Error,
+    fs::File,
+};
+
+// #[feature(core_intrinsics)]
+// fn print_type_of<T>(_: &T) {
+//     println!("{}", unsafe { std::intrinsics::type_name::<T>() });
+// }
 
 struct Player {
     name: String,
@@ -17,6 +27,48 @@ struct Stats {
     physics: u8,
 }
 
-fn main() {
+const PATH: &str = "../Fifa22.csv";
+
+fn main() -> Result<(), Box<dyn Error>> {
     let mut data: Vec<Player> = Vec::new();
+
+    let file = File::open(PATH)?;
+    let mut rdr = csv::Reader::from_reader(file);
+    for result in rdr.records() {
+        let record = result?;
+        // println!("{:?}", record);
+        // print_type_of(&record);
+
+        let map_player = |player: csv::StringRecord| -> Player {
+            let result = Player{
+                name: (&player[0]).to_string(),
+                rating: (&player[1]).to_string().parse().unwrap(),
+                position: (&player[2]).to_string(),
+                nationality: (&player[4]).to_string(),
+                link: (&player[11]).to_string(),
+                stats: Stats {
+                    pace: (&player[5]).to_string().parse().unwrap(),
+                    shoot: (&player[6]).to_string().parse().unwrap(),
+                    pass: (&player[7]).to_string().parse().unwrap(),
+                    dribbling: (&player[8]).to_string().parse().unwrap(),
+                    defend: (&player[9]).to_string().parse().unwrap(),
+                    physics: (&player[10]).to_string().parse().unwrap()
+                }
+            };
+            result
+        };
+
+        let player = map_player(record);
+        data.push(thread::spawn(move || -> Player {
+            player
+        }));
+        // break;
+    }
+
+    let final_result = data.into_iter();
+
+    println!("Final first result: {}", final_result[1]);
+    println!("Final result len: {}", final_result.len());
+
+    Ok(())
 }
